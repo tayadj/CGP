eb1603e38e7086d2733d73a5b4f1d082a5b1e172
tayadj <144615941+tayadj@users.noreply.github.com>
2023-09-17 21:54:54 +0300

    Version 1.1 (6)

--- Lab-1/mainwindow.h
+++ Lab-1/mainwindow.h
@@ -8,46 +8,46 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
-    public:
-        explicit MainWindow(QWidget *parent = nullptr);
-        ~MainWindow();
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
 
-        QColor                      getColor();
-        void                        setColor(QColor);
-        void                        GenerateModals();
+    QColor                      getColor();
+    void                        setColor(QColor);
+    void                        GenerateModals();
 
-    protected slots:
-        void                        MenuModals_Slot();
-        void                        MenuModalsOptions_Slot();
-        void                        MenuInfo_Slot();
-        void                        MenuColor_Slot();
+protected slots:
+    void                        MenuModals_Slot();
+    void                        MenuModalsOptions_Slot();
+    void                        MenuInfo_Slot();
+    void                        MenuColor_Slot();
 
-    private:
-        QColor                      CurrentColor;
+private:
+    QColor                      CurrentColor;
 
-        QGroupBox*                  Tab;
-        QLabel*                     Title;
+    QGroupBox*                  Tab;
+    QLabel*                     Title;
 
-        QGroupBox*                  Menu;
-        QPushButton*                MenuModals;
-        QPushButton*                MenuColor;
-        QPushButton*                MenuInfo;
+    QGroupBox*                  Menu;
+    QPushButton*                MenuModals;
+    QPushButton*                MenuColor;
+    QPushButton*                MenuInfo;
 
-        QGroupBox*                  Info;
-        QLabel*                     InfoTitle;
-        QLabel*                     InfoText;
+    QGroupBox*                  Info;
+    QLabel*                     InfoTitle;
+    QLabel*                     InfoText;
 
-        QGroupBox*                  MenuModalsOptions;
-        QPushButton*                MenuModalsOptionsRGB;
-        QPushButton*                MenuModalsOptionsCMYK;
-        QPushButton*                MenuModalsOptionsHSV;
-        QPushButton*                MenuModalsOptionsHLS;
-        QPushButton*                MenuModalsOptionsXYZ;
-        QPushButton*                MenuModalsOptionsLAB;
+    QGroupBox*                  MenuModalsOptions;
+    QPushButton*                MenuModalsOptionsRGB;
+    QPushButton*                MenuModalsOptionsCMYK;
+    QPushButton*                MenuModalsOptionsHSV;
+    QPushButton*                MenuModalsOptionsHLS;
+    QPushButton*                MenuModalsOptionsXYZ;
+    QPushButton*                MenuModalsOptionsLAB;
 
-        QGroupBox*                  Modals;
-        QVector<QString>*           ModalList = new QVector<QString>;
-        QHBoxLayout*                ModalLayout;
+    QGroupBox*                  Modals;
+    QVector<QString>*           ModalList = new QVector<QString>;
+    QHBoxLayout*                ModalLayout;
 };
 
 #endif
--- Lab-1/modalwindow.h
+++ Lab-1/modalwindow.h
@@ -7,60 +7,60 @@ class ModalWindow : public QGroupBox
 {
     Q_OBJECT
 
-    public:
-        explicit ModalWindow(QWidget *parent = 0, QString Type = "");
-        ~ModalWindow();
+public:
+    explicit ModalWindow(QWidget *parent = 0, QString Type = "");
+    ~ModalWindow();
 
-        void       ReceiveColor();
-        void       SendColor(QColor*);
-        void       RecalculateColor(QColor);
+    void       ReceiveColor();
+    void       SendColor(QColor*);
+    void       RecalculateColor(QColor);
 
-    protected slots:
-        void       RecalculateColor_Slot();
+protected slots:
+    void       RecalculateColor_Slot();
 
-    private:
-        QColor     CurrentColor;
-        QColor     NewColor;
+private:
+    QColor     CurrentColor;
+    QColor     NewColor;
 
-        QLineEdit* RedInput;
-        QLineEdit* GreenInput;
-        QLineEdit* BlueInput;
-        QLineEdit* CyanInput;
-        QLineEdit* MagentaInput;
-        QLineEdit* YellowInput;
-        QLineEdit* KeyInput;
-        QLineEdit* HueInput_1;
-        QLineEdit* SaturationInput_1;
-        QLineEdit* ValueInput;
-        QLineEdit* HueInput_2;
-        QLineEdit* SaturationInput_2;
-        QLineEdit* LightnessInput;
-        QLineEdit* XInput;
-        QLineEdit* YInput;
-        QLineEdit* ZInput;
-        QLineEdit* LInput;
-        QLineEdit* AInput;
-        QLineEdit* BInput;
+    QLineEdit* RedInput;
+    QLineEdit* GreenInput;
+    QLineEdit* BlueInput;
+    QLineEdit* CyanInput;
+    QLineEdit* MagentaInput;
+    QLineEdit* YellowInput;
+    QLineEdit* KeyInput;
+    QLineEdit* HueInput_1;
+    QLineEdit* SaturationInput_1;
+    QLineEdit* ValueInput;
+    QLineEdit* HueInput_2;
+    QLineEdit* SaturationInput_2;
+    QLineEdit* LightnessInput;
+    QLineEdit* XInput;
+    QLineEdit* YInput;
+    QLineEdit* ZInput;
+    QLineEdit* LInput;
+    QLineEdit* AInput;
+    QLineEdit* BInput;
 
-        double Red;
-        double Green;
-        double Blue;
-        double Cyan;
-        double Magenta;
-        double Yellow;
-        double Key;
-        double Hue_1;
-        double Saturation_1;
-        double Value;
-        double Hue_2;
-        double Saturation_2;
-        double Lightness;
-        double X;
-        double Y;
-        double Z;
-        double L;
-        double A;
-        double B;
+    double Red;
+    double Green;
+    double Blue;
+    double Cyan;
+    double Magenta;
+    double Yellow;
+    double Key;
+    double Hue_1;
+    double Saturation_1;
+    double Value;
+    double Hue_2;
+    double Saturation_2;
+    double Lightness;
+    double X;
+    double Y;
+    double Z;
+    double L;
+    double A;
+    double B;
 };
 
 #endif
