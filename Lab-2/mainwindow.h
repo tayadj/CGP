ea209f5c9572c14377a655040df53be78d14326b
tayadj <144615941+tayadj@users.noreply.github.com>
2023-11-12 18:45:23 +0300

    Version 1.0 (6)

---
+++ Lab-2/mainwindow.h
@@ -0,0 +1,33 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QtWidgets>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private:
+    Ui::Widget *ui;
+    bool line_changed=false;
+    QFileSystemModel *model;
+    QGridLayout *backgr;
+    QListView *listView;
+    QTableWidget *twInfo=nullptr;
+     int *sortcolumns = new int[5];
+public slots:
+    void on_listView_doubleClicked(const QModelIndex &index);
+     void twInfoSelected(int logicalIndex);
+private slots:
+    void dialogClose();
+    void on_multiChoice_clicked();
+};
+#endif // WIDGET_H
